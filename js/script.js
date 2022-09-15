85d16b4fdf6446608d1c5f52d71a33e51c2f2c73
lavet13 <lavet13@mail.ru>
2022-09-15 18:12:40 +0300

    confusion of the highest orda

--- js/script.js
+++ js/script.js
@@ -1,39 +1,100 @@
 'use strict';
 
-const getBreakpoint = function () {
-    return window
-        .getComputedStyle(document.body, ':before')
-        .content.replace(/\"/g, '');
-};
+window.addEventListener('DOMContentLoaded', () => {
+    // ELEMENTS
+    const dropDownButton = document.querySelector('.dropdown-btn');
+    const navLinksContainer = document.querySelector('.header__nav-links');
+    const potentialBoxes = document.querySelectorAll('.potential-box');
+    const overlay = document.querySelector('.overlay');
 
-const breakpoints = ['none', 'very small', 'small', 'medium', 'large'];
+    const getBreakpoint = function () {
+        return window
+            .getComputedStyle(document.body, ':before')
+            .content.replace(/\"/g, '');
+    };
 
-const headerIndent = document.querySelector('.header__indent');
+    const breakpoints = ['none', 'very small', 'small', 'medium', 'large'];
 
-const cssApply = function (breakpoint) {
-    switch (breakpoint) {
-        case 'large':
-            break;
+    const headerIndent = document.querySelector('.header__indent');
 
-        case 'medium':
-            break;
-
-        case 'small':
-            console.log('medium');
+    const revealHeaderIndent = function () {
+        if (this) {
             document.querySelector('.header').append(headerIndent);
-            break;
-
-        case 'very small':
+        } else {
             document.querySelector('.header__background').after(headerIndent);
-            break;
+        }
+    };
 
-        case 'none':
-            break;
-    }
-};
+    const cssApply = function (breakpoint) {
+        switch (breakpoint) {
+            case 'large':
+                revealHeaderIndent.call(true);
+                break;
 
-cssApply(getBreakpoint());
+            case 'medium':
+                revealHeaderIndent.call(true);
+                break;
 
-window.addEventListener('resize', () => {
+            case 'small':
+                console.log('medium');
+                revealHeaderIndent.call(true);
+                break;
+
+            case 'very small':
+                revealHeaderIndent.call(false);
+                break;
+
+            case 'tiny':
+                revealHeaderIndent.call(false);
+                break;
+
+            case 'none':
+                revealHeaderIndent.call(false);
+                break;
+        }
+    };
+
     cssApply(getBreakpoint());
+
+    window.addEventListener('resize', () => {
+        cssApply(getBreakpoint());
+    });
+
+    ///////////////////////////////////////////////
+    // DROP_DOWN_BUTTON
+
+    dropDownButton.addEventListener('click', e => {
+        if (e.currentTarget.classList.contains('dropdown-btn--active')) {
+            e.currentTarget.classList.remove('dropdown-btn--active');
+            navLinksContainer.classList.remove('header__nav-links--active');
+            document.body.style.overflow = '';
+        } else {
+            e.currentTarget.classList.add('dropdown-btn--active');
+            navLinksContainer.classList.add('header__nav-links--active');
+            document.body.style.overflow = 'hidden';
+        }
+    });
+
+    /////////////////////////////////////////////////
+    // Reveal elements on scroll
+
+    const revealPotentialBox = function (entries, observer) {
+        const [entry] = entries;
+
+        if (!entry.isIntersecting) return;
+
+        entry.target.classList.remove('potential-box--hidden');
+
+        observer.unobserve(entry.target);
+    };
+
+    const potentialBoxObserver = new IntersectionObserver(revealPotentialBox, {
+        root: null,
+        threshold: 0.42,
+    });
+
+    potentialBoxes.forEach(box => {
+        potentialBoxObserver.observe(box);
+        box.classList.add('potential-box--hidden');
+    });
 });
